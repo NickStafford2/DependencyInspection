d5c42593e1ff77d29ea5b1f88b724def689036e4
Nicholas Stafford <ngstafford1@gmail.com>
2025-01-16 15:55:58 -0500

    renamed component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,33 +2,33 @@ import { useState } from "react";
 import { GraphData, Node } from "./utils/models";
 import Crudbar from "./components/Crudbar";
 import BackendTools from "./components/BackendTools";
-import NpmVisualGraph3d from "./components/NpmVisualGraph3d";
+import DIGraph3d from "./components/NpmVisualGraph3d";
 import { NodeTableContainer } from "./components/table/NodeTableContainer";
 import { columns } from "./components/table/columns";
 
 const App = () => {
-  const [selectedNode, setSelectedNode] = useState<Node>()
-  const manageGraphData = (data: GraphData) => {
-    return data
-  }
-  const onResponseChanged = (data: GraphData) => {
-    // console.log(JSON.parse(JSON.stringify(data)));
-    console.log(data);
-    data = manageGraphData(data)
-    setGraphData(data);
-    setTableData(data.nodes);
-  };
+	const [selectedNode, setSelectedNode] = useState<Node>();
+	const manageGraphData = (data: GraphData) => {
+		return data;
+	};
+	const onResponseChanged = (data: GraphData) => {
+		// console.log(JSON.parse(JSON.stringify(data)));
+		console.log(data);
+		data = manageGraphData(data);
+		setGraphData(data);
+		setTableData(data.nodes);
+	};
 
-  const [graphData, setGraphData] = useState<GraphData>();
-  const [tableData, setTableData] = useState<Node[]>([]);
-  const [scrollTo, setScrollTo] = useState<string>("");
+	const [graphData, setGraphData] = useState<GraphData>();
+	const [tableData, setTableData] = useState<Node[]>([]);
+	const [scrollTo, setScrollTo] = useState<string>("");
 
-  const onNodeSelected = (node: Node) => {
-    setSelectedNode(node)
-    setScrollTo(node.id)
-  }
+	const onNodeSelected = (node: Node) => {
+		setSelectedNode(node);
+		setScrollTo(node.id);
+	};
 
-  /* 
+	/* 
   useEffect(() => {
     if (packageName !== "") {
       const url = `/analyzeNetwork/react`;
@@ -42,21 +42,28 @@ const App = () => {
   }, [packageName]);
   */
 
-  return (
-    <div className="flex flex-col w-full h-full justify-between">
-      <Crudbar onResponse={onResponseChanged} />
-      <div className="flex flex-row grow shrink overflow-hidden">
-        <div className="">
-          <NodeTableContainer columns={columns} data={tableData} scrollTo={scrollTo}></NodeTableContainer>
-        </div>
-        <div className="w-full h-full">
-          <div className="absolute z-10">{selectedNode?.id}</div>
-          <NpmVisualGraph3d graphData={graphData} onNodeSelected={onNodeSelected}></NpmVisualGraph3d>
-        </div>
-      </div>
-      <BackendTools></BackendTools>
-    </div>
-  );
+	return (
+		<div className="flex flex-col w-full h-full justify-between">
+			<Crudbar onResponse={onResponseChanged} />
+			<div className="flex flex-row grow shrink overflow-hidden">
+				<div className="">
+					<NodeTableContainer
+						columns={columns}
+						data={tableData}
+						scrollTo={scrollTo}
+					></NodeTableContainer>
+				</div>
+				<div className="w-full h-full">
+					<div className="absolute z-10">{selectedNode?.id}</div>
+					<DIGraph3d
+						graphData={graphData}
+						onNodeSelected={onNodeSelected}
+					></DIGraph3d>
+				</div>
+			</div>
+			<BackendTools></BackendTools>
+		</div>
+	);
 };
 
 export default App;
