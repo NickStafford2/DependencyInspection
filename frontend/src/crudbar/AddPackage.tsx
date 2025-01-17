6269b810e1e996655ca520dba83cdc186f54d658
Nicholas Stafford <ngstafford1@gmail.com>
2025-01-16 19:21:42 -0500

    made the search bar look much nicer. fixed outline border

--- frontend/src/crudbar/AddPackage.tsx
+++ frontend/src/crudbar/AddPackage.tsx
@@ -17,20 +17,25 @@ export default function AddPackage({
 		}
 	};
 	return (
-		<div className="flex flex-row">
-			<Button
-				className="rounded-r-none border-2 border-r-0"
-				onClick={() => onPackageAdded(addPackageValue)}
-			>
-				Add Package:
-			</Button>
+		<div
+			className="flex flex-row 
+           ring-offset-background file:border-0 rounded-md bg-background  focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm
+      "
+		>
 			<Input
-				className="grow-0 w-64 rounded-l-none border-2 border-l-0"
+				className="grow-0 w-64 rounded-r-none border-2 border-r-0 focus-visible:ring-0"
+				placeholder="Package Search"
 				type="text"
 				value={addPackageValue}
 				onChange={onAddPackageChanged}
 				onKeyDown={handleKeyDown}
 			></Input>
+			<button
+				className="rounded-l-none px-4 border-2 border-l-0 bg-transparent rounded-r-md hover:bg-white/5 font-bold text-lg text-green-300"
+				onClick={() => onPackageAdded(addPackageValue)}
+			>
+				+
+			</button>
 		</div>
 	);
 }
