7c42208beaebced5b4ec393286496d7168bb8043
Nicholas Stafford <ngstafford1@gmail.com>
2025-01-19 22:33:56 -0500

    created asgi.py file

---
+++ backend/dependencyinspection/asgi.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import asyncio
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
+from dependencyinspection import create_app()
+
+app = asyncio.run(create_app())
+
+if __name__ == "__main__": 
+    app.run()
