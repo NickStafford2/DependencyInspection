d9c1c5a9a32326f553daf69a42eb8a1cd483b0b4
Nicholas Stafford <ngstafford1@gmail.com>
2025-01-19 22:34:55 -0500

    fixed syntax error

--- backend/dependencyinspection/asgi.py
+++ backend/dependencyinspection/asgi.py
@@ -5,9 +5,9 @@ import asyncio
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
-from dependencyinspection import create_app()
+from dependencyinspection import create_app
 
 app = asyncio.run(create_app())
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     app.run()
