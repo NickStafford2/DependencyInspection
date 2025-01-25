d56c35c90d4b646e8660d697f8cac212b58b27a2
Nicholas Stafford <ngstafford1@gmail.com>
2025-01-25 17:25:15 -0500

    reading secrets strignt from file

--- backend/dependencyinspection/config.py
+++ backend/dependencyinspection/config.py
@@ -3,21 +3,33 @@ from dynaconf import settings
 
 
 def is_docker() -> bool:
+    print(settings)
     return "IS_DOCKER" in settings
 
 
 def get_overrides() -> dict[str, str]:
     if settings["IS_DOCKER"]:
         return {"NEO4J_PASSWORD": _get_docker_secret()}
+    print(settings)
     return {}
 
 
 def get_config() -> dict[str, str]:
     if settings["IS_DOCKER"]:
         settings["NEO4J_PASSWORD"] = _get_docker_secret()
+    print(settings)
     return settings
 
 
 def _get_docker_secret() -> str:
-    secret_value = os.getenv("NEO4J_PASSWORD") or "docker secret not set"
-    return secret_value
+    try:
+        with open("../neo4j_auth.txt", "r") as file:
+            secret_value = file.read()
+            secret_value = secret_value.split("/")[-1]
+        return secret_value
+    except FileNotFoundError:
+        print("The file ../neo4j_auth.txt was not found.")
+        return "neo4j"
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return "neo4j"
