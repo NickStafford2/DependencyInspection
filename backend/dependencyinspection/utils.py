09adb336b95d6c66c1d2a4cdbcbeed15ee4fe92b
Nicholas Stafford <ngstafford1@gmail.com>
2025-01-27 23:24:53 -0500

    removed previous log not within context

--- backend/dependencyinspection/utils.py
+++ backend/dependencyinspection/utils.py
@@ -12,7 +12,7 @@ T = TypeVar("T")
 
 
 def dev_only(f):
-    logging.debug(f"dev_only() called. {current_app.config}")
+    # logging.debug(f"dev_only() called. {current_app.config}")
 
     def wrapper(*args, **kwargs):
         logging.debug(f"dev_only() called. {current_app.config}")
