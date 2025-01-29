83f646c99bfff0351d5d6510ac840170ec01ac14
Nicholas Stafford <ngstafford1@gmail.com>
2025-01-29 01:37:55 -0500

    made routes dev only

--- backend/dependencyinspection/data/routes.py
+++ backend/dependencyinspection/data/routes.py
@@ -1,4 +1,5 @@
 from dependencyinspection.commonpackages import get_popular_package_names
+from dependencyinspection.config import dev_only
 from dependencyinspection.data import bp
 from dependencyinspection.models import PackageData
 from dependencyinspection.utils import get_all_package_names
@@ -13,50 +14,51 @@ from . import database, scraper
 ########################################################
 
 
-@bp.route("/test")
-async def test():
-    print("success")
-    return "success"
-
-
 @bp.route("/getDBPackages")
 async def get_packages(package_names: list[str]) -> dict[str, PackageData]:
+    dev_only()
     found: dict[str, PackageData] = database.db_search_packages(set(package_names))
     return found
 
 
 @bp.route("/getDBPopularPackages")
 async def get_popular_packages() -> dict[str, PackageData]:
+    dev_only()
     to_search = get_popular_package_names()
     return await get_packages(list(to_search))
 
 
 @bp.route("/getAllDBPackages")
 async def get_all_packages() -> dict[str, PackageData]:
+    dev_only()
     to_search = get_all_package_names()
     return await get_packages(list(to_search))
 
 
 @bp.route("/getDBPackage")
 async def get_package(package_name: str) -> dict[str, PackageData]:
+    dev_only()
     return await get_packages(list(package_name))
 
 
 ########################################################
 @bp.route("/scrapePackages")
 async def scrape_packages(package_names: list[str]) -> str:
+    dev_only()
     found = scraper.scrape_packages(set(package_names))
     return f"Successfully scraped {len(found)} packages.\n"
 
 
 @bp.route("/scrapePopularPackages")
 async def scrape_popular_packages() -> str:
+    dev_only()
     to_search = get_popular_package_names()
     return await scrape_packages(list(to_search))
 
 
 @bp.route("/scrapeAllPackages")
 async def scrape_all_packages() -> str:
+    dev_only()
     to_search = get_all_package_names(999)
     names_in_db = database.get_db_all_names()
     print("yes")
@@ -66,4 +68,5 @@ async def scrape_all_packages() -> str:
 
 @bp.route("/scrapePackage/<package_name>")
 async def scrape_package(package_name: str) -> str:
+    dev_only()
     return await scrape_packages([package_name])
