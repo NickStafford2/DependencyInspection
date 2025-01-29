81bf9e38503c2db3f24cb2c0ff7502d7c72f0b4a
Nicholas Stafford <ngstafford1@gmail.com>
2025-01-29 03:27:51 -0500

    fixed first page load. memo.

--- frontend/src/crudbar/QuerySearch.tsx
+++ frontend/src/crudbar/QuerySearch.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import { GraphData } from "@/utils/models";
 import { Button } from "@/components/ui/button";
 // import { fetchGraphData } from "@/crudbar/api";
@@ -21,35 +21,38 @@ export default function QuerySearch({
 	// 	});
 	// };
 	const removePackage = (name: string) => {
-		console.log(name);
+		// console.log(name);
 		// const oldQuery = query;
-		console.log(query);
+		// console.log(query);
 		query.packages.delete(name);
 		setQuery(query);
 		setQueryUrl(query.toUrl());
-		console.log(query);
+		// console.log(query);
 	};
-	const addPackage = (name: string) => {
-		if (name !== "") {
-			query.packages.add(name);
-			setQuery(query);
-			setQueryUrl(query.toUrl());
-		}
-	};
+	const addPackage = useCallback(
+		(name: string) => {
+			if (name !== "") {
+				query.packages.add(name);
+				setQuery(query);
+				setQueryUrl(query.toUrl());
+			}
+		},
+		[query, setQueryUrl, setQuery],
+	);
 
-	const startSSEConnection = () => {
-		console.log(queryUrl);
-		console.log("startSSEConnection");
+	const startSSEConnection = useCallback(() => {
+		// console.log(queryUrl);
+		// console.log("startSSEConnection");
 		if (!sse) {
 			const sseConnection = new EventSource(query.toUrl(), {
 				withCredentials: false,
 			});
 			sseConnection.onmessage = (e) => {
-				console.log("Received data:", e);
+				// console.log("Received data:", e);
 				setMessages((prevMessages) => [...prevMessages, e.lastEventId]);
 				if (e.lastEventId == "network") {
 					const graphData = JSON.parse(e.data);
-					console.log(graphData);
+					// console.log(graphData);
 					onResponse(graphData);
 				}
 			};
@@ -57,14 +60,14 @@ export default function QuerySearch({
 				console.error("SSE error:", e);
 				sseConnection.close();
 				setSse(null);
-				console.log(messages);
+				// console.log(messages);
 			};
 			sseConnection.onopen = (e) => {
 				console.log("SSE open ", e);
 			};
 			setSse(sseConnection);
 		}
-	};
+	}, [setSse, messages, onResponse, query, queryUrl, sse]);
 
 	useEffect(() => {
 		return () => {
@@ -86,33 +89,36 @@ export default function QuerySearch({
 	}, [addPackage, startSSEConnection]);
 
 	return (
-		<div className="flex flex-row gap-2 items-stretch">
-			{/* <span className="text-white">URL: '{queryUrl}'</span> */}
-			<div className="flex flex-col gap-2 grow-0">
-				<AddPackage onPackageAdded={addPackage} />
-				{query.packages.size ? (
-					<div className="flex flex-row w-64 flex-wrap justify-items-stretch gap-1">
-						<span className="text-white whitespace-nowrap ">
-							{query.packages.size > 1 ? "Seed Nodes:" : "Seed Nodes:"}
-						</span>
-						{Array.from(query.packages).map((name, index) => (
-							<PackageTag
-								className="flex-grow"
-								name={name}
-								onClose={removePackage}
-								key={index}
-							/>
-						))}
-					</div>
-				) : (
-					""
-				)}
-			</div>
-			<div className="">
-				{/* <Button className="h-full " onClick={callBackend}> */}
-				<Button className="h-full " onClick={startSSEConnection}>
-					Search
-				</Button>
+		<div className="flex flex-col gap-2 ">
+			<h3 className="text-xl">Create a New Query:</h3>
+			<div className="flex flex-row gap-2 items-stretch">
+				{/* <span className="text-white">URL: '{queryUrl}'</span> */}
+				<div className="flex flex-col gap-2 grow-0">
+					<AddPackage onPackageAdded={addPackage} />
+					{query.packages.size ? (
+						<div className="flex flex-row w-64 flex-wrap justify-items-stretch gap-1">
+							<span className="text-white whitespace-nowrap ">
+								{query.packages.size > 1 ? "Seed Nodes:" : "Seed Nodes:"}
+							</span>
+							{Array.from(query.packages).map((name, index) => (
+								<PackageTag
+									className="flex-grow"
+									name={name}
+									onClose={removePackage}
+									key={index}
+								/>
+							))}
+						</div>
+					) : (
+						""
+					)}
+				</div>
+				<div className="">
+					{/* <Button className="h-full " onClick={callBackend}> */}
+					<Button className="" onClick={startSSEConnection}>
+						Search
+					</Button>
+				</div>
 			</div>
 		</div>
 	);
