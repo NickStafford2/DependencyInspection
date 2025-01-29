7952ee4caf73f7b513ee08c7438c70a8552c801c
Nicholas Stafford <ngstafford1@gmail.com>
2025-01-29 06:44:51 -0500

    style change for header

--- frontend/src/crudbar/Crudbar.tsx
+++ frontend/src/crudbar/Crudbar.tsx
@@ -11,45 +11,6 @@ import {
 } from "@/components/ui/hover-card";
 
 function Crudbar({ onResponse }: { onResponse: (data: GraphData) => void }) {
-	// const [messages, setMessages] = useState<string[]>([]);
-	// const [sse, setSse] = useState<EventSource | null>(null); // State to manage the EventSource connection
-
-	// const startSSEConnection = () => {
-	// 	console.log("startSSEConnection");
-	// 	if (!sse) {
-	// 		const sseConnection = new EventSource("api/sse", {
-	// 			withCredentials: false,
-	// 		});
-	// 		sseConnection.onmessage = (e) => {
-	// 			console.log("Received data:", e.data);
-	// 			setMessages((prevMessages) => [...prevMessages, e.data]);
-	// 		};
-	// 		sseConnection.onerror = (e) => {
-	// 			console.error("SSE error:", e);
-	// 			sseConnection.close();
-	// 			setSse(null);
-	// 		};
-	// 		sseConnection.onopen = (e) => {
-	// 			console.log("SSE open ", e);
-	// 		};
-	// 		setSse(sseConnection);
-	// 	}
-	// };
-	//
-	// useEffect(() => {
-	// 	return () => {
-	// 		if (sse) {
-	// 			sse.close();
-	// 		}
-	// 	};
-	// }, [sse]);
-
-	// const getPopularNetwork = async () => {
-	// 	fetchGraphData("api/getPopularNetworks").then((data) => {
-	// 		if (data) onResponse(data);
-	// 	});
-	// };
-
 	const getAllDBNetworks = async () => {
 		fetchGraphData("api/getAllDBNetworks").then((data) => {
 			if (data) onResponse(data);
@@ -57,7 +18,7 @@ function Crudbar({ onResponse }: { onResponse: (data: GraphData) => void }) {
 	};
 
 	return (
-		<nav className="px-3 py-2 flex flex-row justify-between items-center gap-3 bg-gradient-to-b from-black to-gray-800  w-full">
+		<nav className="px-3 py-2 flex flex-row justify-between items-start gap-3 bg-gradient-to-b from-black to-gray-800  w-full">
 			<div className="flex flex-row flex-shrink flex-grow-0 ">
 				<h1 className="text-white text-3xl whitespace-nowrap ">
 					Dependency Inspection
