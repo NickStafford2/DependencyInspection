f0cafc11b01876463f708489e80148ad3ebf2895
Nicholas Stafford <ngstafford1@gmail.com>
2025-02-14 18:22:32 -0500

    fixed input style to style container on focus

--- frontend/src/components/table/RowFilter.tsx
+++ frontend/src/components/table/RowFilter.tsx
@@ -3,12 +3,12 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 import { GlobalStateContext } from "@/context";
-import { Input } from "@/components/ui/input";
 import { Table } from "@tanstack/react-table";
+import { PackageNode } from "@/utils/models";
 
 export interface RowFilterProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
-  table: Table<TData>;
+  table: Table<PackageNode>;
 }
 
 /** @useSignals **/
@@ -36,26 +36,28 @@ const RowFilter = React.forwardRef<HTMLDivElement, RowFilterProps>(
       <div
         ref={ref}
         className="flex flex-row w-64
-           ring-offset-background file:border-0 rounded-md bg-background  focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm
+           ring-offset-background file:border-0   border-2 focus-visible:ring-0 rounded-md bg-background  focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm
       "
       >
-        <Input
+        <input
           ref={inputRef}
           placeholder="Filter packages..."
           onChange={onChange}
           value={inputValue}
           className={cn(
             className,
-            "grow-0  rounded-r-none border-2 border-r-0 focus-visible:ring-0",
+            "bg-background flex h-10 w-full rounded-md  px-3 py-2 text-base placeholder:text-muted-foreground focus-within:outline-none focus-within:ring-0",
           )}
           {...props}
         />
-        <button
-          className="rounded-l-none px-4 border-2 border-l-0 bg-transparent rounded-r-md hover:bg-white/5 font-bold text-lg text-gray-700"
-          onClick={clearClicked}
-        >
-          x
-        </button>
+        {inputValue.length === 0 || (
+          <button
+            className="px-4 bg-transparent rounded-r-md hover:bg-white/5 font-bold text-md text-gray-700"
+            onClick={clearClicked}
+          >
+            x
+          </button>
+        )}
       </div>
     );
   },
