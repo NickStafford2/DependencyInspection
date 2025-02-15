0553d3b61e3dce82fe9debe7c9147ccdd9fff71f
Nicholas Stafford <ngstafford1@gmail.com>
2025-02-15 13:10:09 -0500

    refactored HeaderProps into custom type.

--- frontend/src/components/table/columns.tsx
+++ frontend/src/components/table/columns.tsx
@@ -4,22 +4,23 @@ import {
   createColumnHelper,
   CellContext,
 } from "@tanstack/react-table";
-
-import { CellDependencies } from "./CellDependencies";
-// import { CellDependenciesRecursive } from "./CellDependenciesRecursive";
 import { PackageNode } from "@/utils/models";
 import { NodeTableHeader } from "./NodeTableHeader";
+import { CellDependencies } from "./CellDependencies";
+// import { CellDependenciesRecursive } from "./CellDependenciesRecursive";
 
 const columnHelper = createColumnHelper<PackageNode>();
-
 const formatNumber = (value: number, decimals: number) => {
   return value.toFixed(decimals);
+};
+type HeaderProps = {
+  column: Column<PackageNode>;
 };
 
 export const columns: ColumnDef<PackageNode>[] = [
   columnHelper.accessor("id", {
     id: "id",
-    header: ({ column }: { column: Column<PackageNode> }) => {
+    header: ({ column }: HeaderProps) => {
       return <NodeTableHeader column={column} title="Package" />;
     },
     // If 'id' is not a number, no formatting needed.
@@ -27,7 +28,7 @@ export const columns: ColumnDef<PackageNode>[] = [
   }),
   columnHelper.accessor("inDegree", {
     id: "inDegree",
-    header: ({ column }: { column: Column<PackageNode> }) => {
+    header: ({ column }: HeaderProps) => {
       return (
         <NodeTableHeader
           column={column}
@@ -40,7 +41,7 @@ export const columns: ColumnDef<PackageNode>[] = [
 
   columnHelper.accessor("outDegree", {
     id: "outDegree",
-    header: ({ column }: { column: Column<PackageNode> }) => {
+    header: ({ column }: HeaderProps) => {
       return (
         <NodeTableHeader
           column={column}
@@ -52,7 +53,7 @@ export const columns: ColumnDef<PackageNode>[] = [
   }),
   columnHelper.accessor("dependencies", {
     id: "dependencies",
-    header: ({ column }: { column: Column<PackageNode> }) => {
+    header: ({ column }: HeaderProps) => {
       return <NodeTableHeader column={column} title="Dependencies" />;
     },
     cell: (cell: CellContext<PackageNode, string[]>) => {
@@ -62,73 +63,73 @@ export const columns: ColumnDef<PackageNode>[] = [
   }),
   columnHelper.accessor("dependencyOf", {
     id: "dependencyOf",
-    header: ({ column }: { column: Column<PackageNode> }) => {
+    header: ({ column }: HeaderProps) => {
       return <NodeTableHeader column={column} title="Dependency Of" />;
     },
 
     cell: (cell: CellContext<PackageNode, string[]>) => {
-      return <CellDependencies cell={cell}></CellDependencies>;
+      return <CellDependencies cell={cell} />;
       // return <span>test</span>;
     },
   }),
   columnHelper.accessor("allDependencyOf", {
     id: "allDependencyOf",
-    header: ({ column }: { column: Column<PackageNode> }) => {
+    header: ({ column }: HeaderProps) => {
       return <NodeTableHeader column={column} title="All Dependency Of" />;
     },
     cell: (cell) => {
-      return <CellDependencies cell={cell}></CellDependencies>;
+      return <CellDependencies cell={cell} />;
       // return <span>test</span>;
     },
   }),
   columnHelper.accessor("allDependencies", {
     id: "all_dependencies",
-    header: ({ column }: { column: Column<PackageNode> }) => {
+    header: ({ column }: HeaderProps) => {
       return <NodeTableHeader column={column} title="All Dependencies" />;
     },
     cell: (cell) => {
-      return <CellDependencies cell={cell}></CellDependencies>;
+      return <CellDependencies cell={cell} />;
       // return <span>test</span>;
     },
   }),
   columnHelper.accessor("closenessCentrality", {
     id: "closenessCentrality",
-    header: ({ column }: { column: Column<PackageNode> }) => {
+    header: ({ column }: HeaderProps) => {
       return <NodeTableHeader column={column} title="Closeness Centrality" />;
     },
     cell: (info) => formatNumber(info.getValue(), 4),
   }),
   {
     accessorKey: "eigenvectorCentrality",
-    header: ({ column }: { column: Column<PackageNode> }) => {
+    header: ({ column }: HeaderProps) => {
       return <NodeTableHeader column={column} title="Closeness Centrality" />;
     },
     cell: (info) => formatNumber(info.getValue(), 4),
   },
   {
     accessorKey: "clusteringCoefficient",
-    header: ({ column }: { column: Column<PackageNode> }) => {
+    header: ({ column }: HeaderProps) => {
       return <NodeTableHeader column={column} title="Clustering Coefficient" />;
     },
     cell: (info) => formatNumber(info.getValue(), 3),
   },
   {
     accessorKey: "pagerank",
-    header: ({ column }: { column: Column<PackageNode> }) => {
+    header: ({ column }: HeaderProps) => {
       return <NodeTableHeader column={column} title="Pagerank" />;
     },
     cell: (info) => formatNumber(info.getValue(), 5),
   },
   {
     accessorKey: "betweennessCentrality",
-    header: ({ column }: { column: Column<PackageNode> }) => {
+    header: ({ column }: HeaderProps) => {
       return <NodeTableHeader column={column} title="Betweenness Centrality" />;
     },
     cell: (info) => formatNumber(info.getValue(), 4),
   },
   {
     accessorKey: "isSeed",
-    header: ({ column }: { column: Column<PackageNode> }) => {
+    header: ({ column }: HeaderProps) => {
       return <NodeTableHeader column={column} title="Is Seed" />;
     },
     cell: (info) => {
