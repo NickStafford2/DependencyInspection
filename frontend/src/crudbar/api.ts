db0a64a924e93e68e42eae1e72e21e186ba3e697
Nicholas Stafford <ngstafford1@gmail.com>
2025-02-15 13:34:02 -0500

    very minor change to return statement.

--- frontend/src/crudbar/api.ts
+++ frontend/src/crudbar/api.ts
@@ -1,22 +1,22 @@
 import { GraphData, isGraphData } from "@/utils/models";
 
 export const fetchGraphData = async (
-	url: string,
+  url: string,
 ): Promise<GraphData | null> => {
-	try {
-		const response = await fetch(url);
-		if (!response.ok) {
-			const message = `Failed to call ${url}`;
-			// setResponseMessage(message);
-			throw new Error(message);
-		}
-		const data = await response.json();
-		if (isGraphData(data)) {
-			return data;
-		}
-		// setResponseMessage(data);
-	} catch (err) {
-		console.error(err);
-	}
-	return null;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      const message = `Failed to call ${url}`;
+      // setResponseMessage(message);
+      throw new Error(message);
+    }
+    const data = await response.json();
+    if (isGraphData(data)) {
+      return data;
+    }
+    // setResponseMessage(data);
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
 };
