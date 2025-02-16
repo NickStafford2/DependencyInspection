d815161f7d7e715ffe32bd4168e0535c8f58e363
Nicholas Stafford <ngstafford1@gmail.com>
2025-02-16 11:18:50 -0500

    updated welcome

--- frontend/src/sidebar/Sidebar.tsx
+++ frontend/src/sidebar/Sidebar.tsx
@@ -39,20 +39,16 @@ export default function Sidebar() {
             </p>
             <h1 className="pt-4 text-3xl">Incoming Features</h1>
             <h2 className="ml-4 text-xl">Already working on the backend.</h2>
-            <ol className="list-disc ml-8">
-              <li>List of indirect dependencies of each package</li>
-              <li>List of packages that indirectly depend on each package</li>
-              <li>List of direct dependencies of each package</li>
-              <li>List of direct packages that depend on each package</li>
+            <ul className="list-disc ml-8">
               <li>Data Scraping from Github</li>
               <li>Liscense info and threat level.</li>
-            </ol>
+            </ul>
             <h2 className="text-xl ml-4">Longer Term Goals</h2>
-            <ol className="list-disc ml-8">
-              <li>Depreciation Status</li>
-              <li>Rugpull Threat assessment</li>
-              <li>Current NPM Audit status</li>
-            </ol>
+            <ul className="list-disc ml-8">
+              <li>Depreciation status</li>
+              <li>Rugpull risk assessment</li>
+              <li>Current NPM audit status</li>
+            </ul>
           </div>
         </TabsContent>
 
