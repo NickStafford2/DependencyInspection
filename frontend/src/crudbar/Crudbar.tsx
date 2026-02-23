7b0211907bce11568323ce11b787538dffe4208e
Nicholas Stafford <ngstafford1@gmail.com>
2026-02-22 21:09:48 -0500

    changed crudbar to delete beta button

--- frontend/src/crudbar/Crudbar.tsx
+++ frontend/src/crudbar/Crudbar.tsx
@@ -29,30 +29,10 @@ function Crudbar() {
   return (
     <nav className="px-3 py-2 flex flex-row justify-between items-start gap-3 border-b border-border w-full">
       <div className="flex flex-row flex-shrink flex-grow-0 ">
-        <h1 className="text-white text-3xl whitespace-nowrap ">
+        <h1 className="text-white text-3xl whitespace-wrap ">
           Dependency Inspection
         </h1>
-        <HoverCard>
-          <HoverCardTrigger asChild>
-            <Button variant="link">
-              <h2 className="text-red-600 text-4xl font-bold rotate-[-15deg]">
-                BETA
-              </h2>
-            </Button>
-          </HoverCardTrigger>
-          <HoverCardContent>
-            <div className="w-100 max-w-100">
-              <h3 className="text-red-600 text-2xl font-semibold w-30 max-w-30">
-                Ongoing Development.
-              </h3>
-              <p className="py-2">
-                Most of the impressive stuff is on the backend. I am currently
-                working on the frontend to show all the incredible data I
-                already have on the backend.{" "}
-              </p>
-            </div>
-          </HoverCardContent>
-        </HoverCard>
+
       </div>
       <QuerySearch />
       <div className="flex flex-col gap-4">
