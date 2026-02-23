c882dba6904fe43b05b52b615c38fc38d6ab2a98
Nicholas Stafford <ngstafford1@gmail.com>
2026-02-23 01:48:19 -0500

    updated description.

--- frontend/src/sidebar/Search.tsx
+++ frontend/src/sidebar/Search.tsx
@@ -4,24 +4,14 @@
 function Search() {
   return (
     <div className="p-4 flex flex-col gap-2">
-      <h1 className="text-3xl">Welcome. :)</h1>
+      <h1 className="text-3xl">Welcome</h1>
       <p>
-        Use this tool to review the dependencies of your npm packages. Query any
+        Review the dependencies of your npm packages. Query any
         number of packages, and DependencyInspection build a network of all the
-        dependencies, their vulnerabilities, and all sorts of information.
+        dependencies, their vulnerabilities, and all sorts of network information.
       </p>
-      <h1 className="pt-4 text-3xl">Incoming Features</h1>
-      <h2 className="ml-4 text-xl">Already working on the backend.</h2>
-      <ul className="list-disc ml-8">
-        <li>Data Scraping from Github</li>
-        <li>Liscense info and threat level.</li>
-      </ul>
-      <h2 className="text-xl ml-4">Longer Term Goals</h2>
-      <ul className="list-disc ml-8">
-        <li>Depreciation status</li>
-        <li>Rugpull risk assessment</li>
-        <li>Current NPM audit status</li>
-      </ul>
+      <h1 className="pt-4 text-3xl">Try it out</h1>
+      <p>In the search bar at the top of the screen. Type in at least one npm package. Search, and we will dynamically load the entire network of dependencies. </p>
       {/* <QuerySearch /> */}
     </div>
   );
